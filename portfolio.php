8ec4745a3745a3d19cfb0d4848e1c305e2e25115
Alok mishra <alokmishra11.am@gmail.com>
2018-03-28 13:13:37 -0500

    portfolio
    fst portfolio

---
+++ portfolio.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Musicsapiens</title>
+	<meta name="viewport" content-type="width=device-width,initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="content-type" content="text/html" charset="utf-8">
+		<link rel="icon" type="image/png" href="http://localhost:8080/websites projects/pics/musiclogo2.png">
+		<link rel="stylesheet" type="text/css" href="landingpage.css">
+		<link rel="stylesheet" type="text/css" href="animate.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+</head>
+<body style="background-color:   #CAD2EA  ;">
+	<!--Creates the popup body-->
+	<!-- Modal for image -->
+	
+	<div id="myModal" style="position: fixed;">
+		<h2 style="color:red; margin-left: 50px;">.....This is just to show the designing work</h2>
+  		<div class="img_modal">
+  			<img src ="http://localhost:8080/websites projects/pics/emax.jpg" class="clg_img">
+  		</div>
+  	</div>
+
+
+	<div id="top_bar">
+	<nav class="navbar navbar-inverse" style="background-color: #2A2C34    ;">
+		<h2 class="heading" ><strong style="text-shadow: 1px  1px 1px #9D5D05;">Alok</strong><i> mishra</i> </h2>
+		<div class="Menus">
+		<ul class="nav nav-pills">
+			  <li><a href="" class="btn btn-info btn-lg" style="border:none;">
+          <span class="glyphicon glyphicon-envelope" style="padding:5px 15px;"></span>E-mail
+        </a></li>
+			  <li><a href="https://www.facebook.com/alokmishra3751"><img src="http://localhost:8080/websites projects/pics/fb3.png" style="width: 40px; height: 40px; padding:2px 2px;">Connect</a></li>
+			  <li><a href="#" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-earphone" style="padding:5px 5px;"></span><b>+91</b> 8527932207</a></li>
+			  <li><a href="#">Projects</a></li>
+			  <li><a href="#">Know more..</a></li>
+		</ul>
+		</div>
+	</nav>
+	</div>
+
+	<div id ="cover_col">
+		<img src="http://localhost:8080/websites projects/pics/alok.jpg" class="my_img">
+		<div class="lines">
+			<div class="bar">
+			<span class="glyphicon glyphicon-triangle-right"></span><p class="my_name">ALOK MISHRA</p></div>
+			<div class="bar">
+			<span class="glyphicon glyphicon-triangle-right"></span><p class="line2">Frontend developer<br>portfolio</p></div>
+			<div class="bar"> 
+			<span class="glyphicon glyphicon-triangle-right"></span>
+			<p class="E_mail">E-mail:-<a href="#" class="email" >alokmishra11.am@gmail.com</a></p>
+			</div>
+		</div> 
+	</div>
+
+	<!--aboutme column-->
+
+
+	<div id="Aboutme_col">
+		<h2 class="about_me">About me</h2>
+		<div class="aboutme_col2">
+			<h3>I'm a self taught Front End Developer from Delhi, India.</h3>
+
+			<p class="aboutme_col4">I enjoy turning complex problems into simple, beautiful and intuitive interface designs. When I'm not coding or pushing pixels, you'll find me in the gym or in the sports yard.</p>
+			<p> This is my first portfolio on a beginner level of <B>            Frontend development
+		</div>
+		<div class="hobbies_col">
+			<h4>Other Activities</h4>
+			<img src ="http://localhost:8080/websites projects/pics/volly.png">
+			<img src ="http://localhost:8080/websites projects/pics/soccer.png">
+			<img src ="http://localhost:8080/websites projects/pics/mjd.png">
+			<img src ="http://localhost:8080/websites projects/pics/musiclogo2.png">
+			<img src ="http://localhost:8080/websites projects/pics/read.png">
+			<img src ="http://localhost:8080/websites projects/pics/gaming.png">
+		</div>
+	</div>
+
+	<!--education column-->
+
+	<div id="Education_col">
+		<h2 class="edu_text">Education & qualification</h2>
+		<div class="grad_col">
+			<fieldset>
+				<legend>Graduation (2012-2016)</legend>
+				<img src="http://localhost:8080/websites projects/pics/graduation.png" style="width: 50px; height: 50px; margin-left: 50px;"><h5>Bachelore of Technology (computer science)</h5>
+				<button class="Scl_img_btn"><img src ="http://localhost:8080/websites projects/pics/emax.jpg" class="scl_img"></button>
+			<div class="edu_col">
+			<div  class="edu_col_lines">University:<p>Kurukshetra University</p></div>
+			<div class="edu_col_lines">College:<p>E-max institute of technology  (ambala, haryana, India-134003)</p></div>
+			<div class="edu_col_lines">Training in html,css,php:<p>Infowiz-A software solution (chandigarh,India-160017)</p></div>
+			<div class="edu_col_lines">Training in javascript:<p>Apex institute (Noida, sector 63 ,India-201301)</p> </div>
+			</div>
+			</fieldset>
+		</div>
+	</div>
+
+	<!--    skills set    -->
+	<div class="thumbnail" style="background-color:  #92A0B0; border:none;">
+		<h2 style="width: 100%; height: 50px; background-color: #9D5D05; color:white; text-align: center; padding:5px; box-shadow: 0px 2px 8px .3px rgba(0,0,0,0.5);"> Skills <b>set</b></h2>
+        <div id="DemoCarousel" class="carousel slide" data-interval="5000" data-ride="carousel">
+            
+            <ol class="carousel-indicators">
+                <li data-target="#DemoCarousel" data-slide-to="0" style="border:1px solid black; padding:8px 8px;"></li>
+                <li data-target="#DemoCarousel" data-slide-to="1" style="border:1px solid black; padding:8px 8px;" ></li>
+                <li data-target="#DemoCarousel" data-slide-to="2" style="border:1px solid black; padding:8px 8px;"></li>
+                <li data-target="#DemoCarousel" data-slide-to="3" style="border:1px solid black; padding:8px 8px;"></li>
+                <li data-target="#DemoCarousel" data-slide-to="4" style="border:1px solid black; padding:8px 8px;"></li>
+
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="carousel-caption">
+                        <img src ="http://localhost:8080/websites projects/pics/htm5.png">
+                    	
+                    
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-caption">
+                        <img src ="http://localhost:8080/websites projects/pics/css3.png">
+                                    	
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-caption">
+                       <img src ="http://localhost:8080/websites projects/pics/js.png"> 
+                                           	              
+                   </div>
+                    
+                </div>
+                <div class="item">
+                    <div class="carousel-caption">
+                       <img src ="http://localhost:8080/websites projects/pics/jquery.png"> 
+                                      
+                   </div>
+                    
+                </div>
+                <div class="item">
+                    <div class="carousel-caption">
+                       <img src ="http://localhost:8080/websites projects/pics/bootstrap.png" style="background-color: white;">
+                    	                  
+                   </div>
+                    
+                </div>
+            </div>
+            <a class="carousel-control left" href="#DemoCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="carousel-control right" href="#DemoCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div>
+    </div>
+	
+
+<script src="http://localhost:8080/websites projects/js/jquery-3.3.1.slim.min .js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://localhost:8080/websites projects/js/plugin.js"></script>
+
+<script>
+	$(document).ready(function(){
+
+
+		
+
+	$(".scl_img").click(function(){
+        $("#myModal").show(1000);
+        $(".img_modal").show();
+        $(".scl_img").css({opacity:"1"});
+
+    });
+    $("#myModal").click(function(){
+    	$("#myModal").hide(1000);
+    	$(".img_modal").hide(300);
+    });
+    $(window).scroll(function(){
+    	if($(this).scrollTop()>200){
+    		$("#Education_col").show();
+    		$(".edu_col_lines").show();
+    		$(".heading").animate({
+    			"font-size":"20px"
+
+    		});
+		    	
+		   }
+    	else{
+    		$(".edu_col_lines").hide();
+    		$("#Education_col").hide();
+    		$(".heading").animate({
+    			"font-size":"25px"
+    		});
+    	}
+    });
+    	
+
+});
+	
+</script>
+
+</body>
+</html>
